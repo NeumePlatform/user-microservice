38a18ed177d589dc3bd9180fa080cc15252771e4
stefan <452766@student.fontys.nl>
2023-05-06 23:39:53 +0200

    Added new reference

--- userData/dataAccess/dataAccess.cs
+++ userData/dataAccess/dataAccess.cs
@@ -8,6 +8,7 @@ namespace userData.dataAccess
         //private const string connectionString = "mongodb://localhost:27016";
         private const string connectionString = "mongodb+srv://stefandeboer:kfY4RyWzQNR1axm0@cluster0.7dakmvs.mongodb.net/?retryWrites=true&w=majority";
 		private const string databaseName = "neume-users";
+
 		private const string userCollection = "users";
 
 		public IMongoCollection<T> ConnectToMongo<T>(in string collection)
